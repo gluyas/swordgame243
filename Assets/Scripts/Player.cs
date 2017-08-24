8bd6fc729e75773ef7d66c469fe2ba175faf8d2a
Marc <marc.gluyas@gmail.com>
2017-08-24 15:57:52 +1200

    Tweaked game balance, fixed UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
-using NUnit.Framework.Constraints;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
